24bac2092c1933242be1f507bce7f06c6b613273
Sheena Aggarwal <Sheena@shl-wp17.vpl.ca>
2023-03-31 15:29:06 -0700

    new config with amazon cognito user pool

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_U1yC066RC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7016oqvvcp3nsgaeqjc8e85ll4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-west-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
