4e178bbc3a26c5ef7a45043756603978e28a8489
Sheena Aggarwal <Sheena@Sheenas-Air.hitronhub.home>
2023-03-31 21:09:36 -0700

    upted region in config

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-west-2_U1yC066RC', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '7016oqvvcp3nsgaeqjc8e85ll4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-west-2
+        region: 'us-west-2' // e.g. us-west-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
